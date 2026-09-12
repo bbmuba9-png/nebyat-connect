5a67f639f9d198e8524a486c8a5144953294e451
bbmuba9-png <bbmuba9@gmail.com>
2026-09-12 15:45:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,49 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
+// ተጠቃሚዎች ስልክ ቁጥራቸውን እና የሶኬት መለያቸውን (Socket ID) ለመያዝ
+const users = {}; 
 
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId);
-        });
+io.on('connection', (socket) => {
+    console.log('ተጠቃሚ ተገናኝቷል (Connected):', socket.id);
+
+    // 1. ተጠቃሚው ስልክ ቁጥሩን ሲመዘግብ
+    socket.on('register-phone', (phoneNumber) => {
+        users[phoneNumber] = socket.id;
+        console.log(`ስልክ ቁጥር ተመዝግቧል: ${phoneNumber} -> ሶኬት ID: ${socket.id}`);
+    });
+
+    // 2. ቀጥተኛ ጥሪ ማድረግ (Direct Call Signaling)
+    socket.on('call-user', ({ targetPhoneNumber, callerPhoneNumber }) => {
+        const targetSocketId = users[targetPhoneNumber];
+
+        if (targetSocketId) {
+            // ጥሪውን ወደ ተቀባዩ ስልክ መላክ (Ringing Event)
+            io.to(targetSocketId).emit('incoming-call', {
+                callerPhoneNumber: callerPhoneNumber,
+                callerSocketId: socket.id
+            });
+            console.log(`ጥሪ ከ ${callerPhoneNumber} ወደ ${targetPhoneNumber} ተልኳል`);
+        } else {
+            // ተጠቃሚው ኦንላይን ካልሆነ
+            socket.emit('call-failed', { message: 'ተጠቃሚው ኦንላይን አይደለም ወይም አልተገኘም!' });
+        }
+    });
+
+    // 3. ጥሪውን መቀበል (Answer Call)
+    socket.on('accept-call', ({ callerSocketId, signal }) => {
+        io.to(callerSocketId).emit('call-accepted', signal);
+    });
+
+    // 4. ተጠቃሚው ሲቋረጥ (Disconnect)
+    socket.on('disconnect', () => {
+        for (let phone in users) {
+            if (users[phone] === socket.id) {
+                delete users[phone];
+                console.log(`ተጠቃሚ ወጥቷል (Disconnected): ${phone}`);
+                break;
+            }
+        }
     });
 });
 
